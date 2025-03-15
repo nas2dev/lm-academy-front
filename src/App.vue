e9da12b21504b5e49665d3d4e6668850e0811326
nas2dev <nas2dev@gmail.com>
2025-03-15 14:15:34 +0100

    Inital Commit for vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <p>{{ message }}</p>
+</template>
+
+<script setup>
+import {ref} from "vue"
+const message = ref("Almedin");
+
+setTimeout(() => {
+  message.value = "Perfundoj kursi per sot!"
+}, 5000);
+</script>
+
+<style>
+p{
+  color: red;
+}
+</style>
